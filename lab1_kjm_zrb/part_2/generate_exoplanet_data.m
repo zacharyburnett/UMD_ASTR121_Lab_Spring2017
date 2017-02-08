0ce387c2f553e6c7467f38547db56be365b5af43
Zachary Burnett <zrb@umd.edu>
2017-02-08 16:14:55 -0500

    Fixed uncertainty propogation: it was applying the uncertainity to the time values instead of to the actual generated data

--- lab1_kjm_zrb/part_2/generate_exoplanet_data.m
+++ lab1_kjm_zrb/part_2/generate_exoplanet_data.m
@@ -1,11 +1,15 @@
 function [ generated_data ] = generate_exoplanet_data( time_values, period, amplitude, error )
 %Generates exoplanet radial velocity curve.
 %   This function accepts period (in days), amplitude (in m/s), and an array of time values,
-%   and generates a sine curve of radial velocity data to simulate data from an exoplanet.
+%   and generates a sine curve of radial velocity data to simulate data from an exoplanet with given error percentage.
 
+% get perfect sine wave data from given time values
+generated_data = amplitude .* sin(time_values .* (2 .* pi ./ period));
+
+% get uncertainties inside the given error range
 uncertainties = random('uniform', -error, error, 1, length(time_values));
 
-noise_values = time_values + (time_values .* uncertainties);
+% apply uncertanties to generated data
+generated_data = generated_data + generated_data .* uncertainties;
 
-generated_data = amplitude .* sin(noise_values .* (2 .* pi ./ period));
 end
--- lab1_kjm_zrb/part_2/plot_exoplanet_data.m
+++ lab1_kjm_zrb/part_2/plot_exoplanet_data.m
@@ -1,6 +1,6 @@
 time_values = 1:500;
 amplitude = 100;
-period = 500;
+period = 100;
 error = 0.2;
 
 clf
