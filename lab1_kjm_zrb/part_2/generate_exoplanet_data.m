b6b0677dcabe7db1986f82f4a9e51bea8ef55330
Zachary Burnett <zrb@umd.edu>
2017-02-08 15:32:56 -0500

    added error to function arguments

--- lab1_kjm_zrb/part_2/generate_exoplanet_data.m
+++ lab1_kjm_zrb/part_2/generate_exoplanet_data.m
@@ -1,9 +1,9 @@
-function [ generated_data ] = generate_exoplanet_data( time_values, period, amplitude )
+function [ generated_data ] = generate_exoplanet_data( time_values, period, amplitude, error )
 %Generates exoplanet radial velocity curve.
 %   This function accepts period (in days), amplitude (in m/s), and an array of time values,
 %   and generates a sine curve of radial velocity data to simulate data from an exoplanet.
 
-uncertainties = random('uniform', -0.2, 0.2, 1, length(time_values));
+uncertainties = random('uniform', -error, error, 1, length(time_values));
 
 noise_values = time_values + (time_values .* uncertainties);
 
--- lab1_kjm_zrb/part_2/plot_exoplanet_data.m
+++ lab1_kjm_zrb/part_2/plot_exoplanet_data.m
@@ -1,10 +1,11 @@
 time_values = 1:500;
 amplitude = 100;
 period = 500;
+error = 0.2;
 
 clf
 hold on
-plot(generate_exoplanet_data(time_values, period, amplitude))
+plot(generate_exoplanet_data(time_values, period, amplitude, error))
 xlabel('Time (days)');
 ylabel('Meters per Second');
 title('Generated Radial Velocity Curve')
