c1c1ac568e93d6da2fc8cb2c17e2024681408b63
Zachary Burnett <zrb@umd.edu>
2017-02-13 14:34:37 -0500

    added error bars and finished first section of 1b

--- lab1_kjm_zrb/part_2/generate_exoplanet_data.m
+++ lab1_kjm_zrb/part_2/generate_exoplanet_data.m
@@ -1,15 +1,9 @@
-function [ generated_data ] = generate_exoplanet_data( time_values, period, amplitude, error )
+function [ generated_data ] = generate_exoplanet_data( time_values, period, amplitude )
 %Generates exoplanet radial velocity curve.
 %   This function accepts period (in days), amplitude (in m/s), and an array of time values,
-%   and generates a sine curve of radial velocity data to simulate data from an exoplanet with given error percentage.
+%   and generates a sine curve of radial velocity data to simulate data from an exoplanet.
 
 % get perfect sine wave data from given time values
 generated_data = amplitude .* sin(time_values .* (2 .* pi ./ period));
-
-% get uncertainties inside the given error range
-uncertainties = random('uniform', -error, error, 1, length(time_values));
-
-% apply uncertanties to generated data
-generated_data = generated_data + generated_data .* uncertainties;
 
 end
--- lab1_kjm_zrb/part_2/plot_exoplanet_data.m
+++ lab1_kjm_zrb/part_2/plot_exoplanet_data.m
@@ -1,11 +1,20 @@
 time_values = 1:365;
 amplitude = 40;
-period = 67;
+period = 147;
 error = 0.2;
 
+sine_wave = generate_exoplanet_data(time_values, period, amplitude);
+
+% get uncertainties inside the given error range
+uncertainties = sine_wave .* random('uniform', -error, error, 1, length(time_values));
+
+% apply uncertanties to generated data
+generated_data = sine_wave + uncertainties;
+
+% plot
 clf
 hold on
-plot(generate_exoplanet_data(time_values, period, amplitude, error))
+errorbar(generated_data, repmat(std(uncertainties), 1, length(generated_data)), '.');
 xlabel('Time (days)');
 ylabel('Meters per Second');
 title('Generated Radial Velocity Curve')
