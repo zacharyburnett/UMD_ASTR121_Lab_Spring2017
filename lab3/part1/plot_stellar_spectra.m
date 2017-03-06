de35d531c54b1438a952985e3e8438be0f5901c0
Zachary Burnett <zrb@umd.edu>
2017-03-06 15:49:08 -0500

    end of lab upload

--- lab3/part1/plot_stellar_spectra.m
+++ lab3/part1/plot_stellar_spectra.m
@@ -1,4 +1,9 @@
 % part 1, plot perfect blackbody spectra of three stars of spectral type
 % M0, G2, and A0
 
-% generate array of random wavelengths
+% generate array of random wavelengths between 0.1 and 1500 nanometers
+num_wavelengths = 100000;
+wavelengths = random('uniform', 1e-10, 1.5e-6, 1, num_wavelengths);
+
+main_sequence = xlsread('main_sequence.csv');
+
