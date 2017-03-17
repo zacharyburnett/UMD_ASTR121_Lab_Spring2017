e84dfb3f43ed103fbc53c03f87788afca50ee951
Zachary Burnett <zrb@umd.edu>
2017-03-17 19:21:58 -0400

    fixed table indexing
    switched parentheses to brackets and transposed sum output

--- lab3/part2/part2b.m
+++ lab3/part2/part2b.m
@@ -6,6 +6,6 @@ residual_sumsquares = array2table(zeros(12, 5), 'VariableNames', unknown_spectra
 
 for current_unknown_spectra = unknown_spectra.Properties.VariableNames
     for current_known_spectra = known_spectra.Properties.VariableNames
-        residual_sumsquares(:, current_unknown_spectra) = num2cell(sum((table2array(unknown_spectra(:, current_unknown_spectra)) - table2array(known_spectra)).^2));
+        residual_sumsquares{:, current_unknown_spectra} = sum((table2array(unknown_spectra(:, current_unknown_spectra)) - table2array(known_spectra)).^2)';
     end
 end
