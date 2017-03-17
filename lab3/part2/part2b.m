18cc5f8c02cb7d0e646313de3a77ff9dccd1e206
Zachary Burnett <zrb@umd.edu>
2017-03-17 12:20:38 -0400

    final

--- lab3/part2/part2b.m
+++ lab3/part2/part2b.m
@@ -2,14 +2,10 @@
 known_spectra = readtable('known_spectra.csv', 'ReadVariableNames', true, 'ReadRowNames', true);
 unknown_spectra = readtable('unknown_spectra.csv', 'ReadVariableNames', true, 'ReadRowNames', true);
 
-% populate data frame of residual sum of squares
-%for ( current_unknown_spectra in colnames(unknown_spectra) )
-%  residual_sumsquares(current_unknown_spectra) = as.vector( colSums(( unknown_spectra(current_unknown_spectra) - known_spectra ) .^2 ));
-
 residual_sumsquares = array2table(zeros(12, 5), 'VariableNames', unknown_spectra.Properties.VariableNames, 'RowNames', known_spectra.Properties.VariableNames);
 
 for current_unknown_spectra = unknown_spectra.Properties.VariableNames
     for current_known_spectra = known_spectra.Properties.VariableNames
-        residual_sumsquares(:, current_unknown_spectra) = sum((table2array(unknown_spectra(:, current_unknown_spectra)) - table2array(known_spectra)).^2);
+        residual_sumsquares(:, current_unknown_spectra) = num2cell(sum((table2array(unknown_spectra(:, current_unknown_spectra)) - table2array(known_spectra)).^2));
     end
 end
