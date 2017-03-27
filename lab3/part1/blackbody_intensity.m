bdc757557fa9fca6250e420b2ccfc352a6797f31
Zachary Burnett <zrb@umd.edu>
2017-03-27 15:52:43 -0400

    changed blackbody intensity function

--- lab3/part1/blackbody_intensity.m
+++ lab3/part1/blackbody_intensity.m
@@ -1,6 +1,7 @@
 function [ intensities ] = blackbody_intensity( wavelengths, temperature )
 %blackbody_intensity Returns intensity in solar irradiance per meter per steradian
 %of a blackbody with the given array of wavelengths and a single temperature.
+
 planck_constant = 6.6626e-34; % Planck's constant (h) in meters squared kilograms per second
 boltzmann_constant = 1.381e-23; % Boltzmann's constant (k_B) in joules per Kelvin
 speed_of_light = 299792458; % Speed of light (c) in meters per second.
