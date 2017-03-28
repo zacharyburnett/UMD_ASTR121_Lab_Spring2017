6aa66cccc7f83fe5540ae8c84b1940ca211eae31
Zach <zrb@umd.edu>
2017-03-28 11:41:45 -0400

    Update temperature_from_colorindex.m with other formula

--- lab4/temperature_from_colorindex.m
+++ lab4/temperature_from_colorindex.m
@@ -1,7 +1,10 @@
 function [ temperature ] = temperature_from_colorindex( color_index )
-%TEMPERATURE_FROM_COLORINDEX Summary of this function goes here
-%   Detailed explanation goes here
+%TEMPERATURE_FROM_COLORINDEX Returns temperature given a color index (B - V)
 
-temperature = 4600 * (1 ./ (0.92 .* (color_index) + 1.7) + 1 ./ (0.92 .* (color_index) + 0.62));
+% formula from http://www.astro.sunysb.edu/fwalter/AST443/b-v_temp.html
+%temperature = 8540 / (color_index + 0.865);
+
+% formula from Wikipedia
+temperature = 4600 * (1 / (0.92 * (color_index) + 1.7) + 1 / (0.92 * (color_index) + 0.62));
+
 end
-
