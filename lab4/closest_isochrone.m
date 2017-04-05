16b51a4f79970742f9f1a9c70e4073e55794b85b
Zachary Burnett <zrb@umd.edu>
2017-04-05 16:54:03 -0400

    finished closest fit

--- lab4/closest_isochrone.m
+++ lab4/closest_isochrone.m
@@ -1,12 +1,23 @@
-function [ best_fit ] = closest_isochrone( cluster_magnitudes, isochrones )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ visual_isochrone_name, blue_isochrone_name ] = closest_isochrone( cluster_magnitudes, isochrones )
+%CLOSEST_ISOCHRONE Returns name of closest isochrone given cluster 
+%magnitudes and isochrone struct
 
-residual_sumsquares = array2table(zeros(12, 5), 'VariableNames', isochrones.Properties.VariableNames);
+% get field names of isochrone struct
+fields = fieldnames(isochrones);
 
-for current_isochrone = isochrones.Properties.VariableNames
-    residual_sumsquares{:, current_isochrone} = sum(.^2)';
+% create table of 0s to store sumsquares
+residual_sumsquares = array2table(zeros(2, length(fields)), 'VariableNames', fields, 'RowNames', {'visual', 'blue'});
+
+for index = 1:numel(fields)
+    current_isochrone = isochrones.(fields{index});
+    visual_mag = interp1(current_isochrone.V, 1:length(cluster_magnitudes.V), 'spline');
+    blue_mag = interp1(current_isochrone.B, 1:length(cluster_magnitudes.B), 'spline');
+    residual_sumsquares{1, index} = nansum((cluster_magnitudes.V - visual_mag').^2);
+    residual_sumsquares{2, index} = nansum((cluster_magnitudes.V - blue_mag').^2);
 end
+
+visual_isochrone_name = residual_sumsquares('visual', residual_sumsquares{'visual',:} == min(residual_sumsquares{'visual',:})).Properties.VariableNames;
+blue_isochrone_name = residual_sumsquares('blue', residual_sumsquares{'blue',:} == min(residual_sumsquares{'blue',:})).Properties.VariableNames;
 
 end
 
