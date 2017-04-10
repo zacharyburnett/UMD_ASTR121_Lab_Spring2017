63f4c823e4cddd4e6c8e689a71577857eb4cd195
Zachary Burnett <zrb@umd.edu>
2017-04-10 15:39:03 -0400

    test

--- lab5/part1b.m
+++ lab5/part1b.m
@@ -1,26 +1,27 @@
-% linear mass
-
-% define mass of galaxy
-galactic_mass = 5.8e11; % Vayntrub, Alina (2000). "Mass of the Milky Way". The Physics Factbook. Archived from the original on August 13, 2014. Retrieved May 9, 2007.4
-
 % define meters in a kiloparsec
 kiloparsec_meters = 3.086e19; % wikipedia
 
-% define total galactic radius
-galactic_radius = 15 * kiloparsec_meters;
+% define mass of galaxy
+galactic_mass = 1e12 * 1.989e30; % Vayntrub, Alina (2000). "Mass of the Milky Way". The Physics Factbook. Archived from the original on August 13, 2014. Retrieved May 9, 2007.4
 
 % get linspace of radii
-radii = linspace(0, galactic_radius, 1000);
+inner_radii = linspace(0, 2 * kiloparsec_meters, 500);
 
-% get velocities
-velocities_m_s = galactic_rotational_velocity(radii, galactic_mass * (radii / galactic_radius));
+% get linspace of radii
+outer_radii = linspace(2 * kiloparsec_meters, 8 * kiloparsec_meters, 500);
 
 % start plotting
 hold on
 
-plot(radii / kiloparsec_meters, velocities_m_s / 1000);
+% plot inside inner sphere
+plot(inner_radii / kiloparsec_meters, galactic_rotational_velocity(inner_radii, galactic_mass * (inner_radii / galactic_radius).^3) / 1000, '-b');
+
+% plot outside inner sphere
+plot(outer_radii / kiloparsec_meters, galactic_rotational_velocity(outer_radii, galactic_mass) / 1000, '-b');
 
 xlabel('Radius (kpc)');
 ylabel('Velocity (km/s)');
+
+%legend('');
 
 hold off
