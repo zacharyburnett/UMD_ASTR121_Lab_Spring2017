d592461fa2bc5b47eb0ff2ecf95b4776722585e6
Zachary Burnett <zrb@umd.edu>
2017-04-20 23:19:19 -0400

    final code

--- lab5/part2b.m
+++ lab5/part2b.m
@@ -41,10 +41,10 @@ solar_orbital_velocity_uncertainty = sqrt(...
     solar_velocity_galactic_longitude_uncertainty^2 * (-solar_orbital_velocity * cos(solar_velocity_galactic_latitude) * sin(solar_velocity_galactic_longitude - deg2rad(90))).^2);
 
 % get linspace of radii inside 2 kpc
-inner_radii = linspace(0, 2 * meters_in_kiloparsec, 500);
+inner_radii = linspace(0, 2 * meters_in_kiloparsec, 200);
 
 % get linspace of radii outside 2 kpc
-outer_radii = linspace(2 * meters_in_kiloparsec, 8 * meters_in_kiloparsec, 500);
+outer_radii = linspace(2 * meters_in_kiloparsec, 8 * meters_in_kiloparsec, 200);
 
 %% read data
 
@@ -139,18 +139,22 @@ orbital_radii_uncertainties = sqrt(...
     solar_orbital_radius_uncertainty^2 .* sin(galactic_longitudes).^2 + ...
     galactic_longitudes_uncertainties.^2 .* (solar_orbital_radius .* cos(galactic_longitudes)).^2);
 
-%% plot rotation curves
+%% Get mass at 2 and 4 kpc
 
 % from cftool with Bisquare remain fit
-%slope = [2.894e+04, 3.369e+04, 3.844e+04];
-%y_intercept = [2.484e+05, 2.575e+05, 2.529e+05];
+slope = 7.3e-16;
+slope_uncertainty = 1.0300e-16;
+y_intercept = 1.473e+05;
+y_intercept_uncertainty = 15600;
 
-slope = 3.369e+04;
-y_intercept = 2.575e+05;
+theoretical_radii = meters_in_kiloparsec:meters_in_kiloparsec:(9 * meters_in_kiloparsec);
 
-mass_at_2_kpc = 1.7e10 * 1.989e30; %(slope / 1000 .* (2) + y_intercept / 1000)^2 * (2 * meters_in_kiloparsec) / cavendish_constant;
+masses = (slope * theoretical_radii + y_intercept).^2 .* theoretical_radii / cavendish_constant;
+masses_uncertainties = sqrt(...
+    slope_uncertainty^2 * (theoretical_radii .* (slope * theoretical_radii + y_intercept)).^2 + ...
+    y_intercept_uncertainty^2 * (slope * theoretical_radii + y_intercept).^2);
 
-masses = orbital_velocities.^2 .* orbital_radii / cavendish_constant;
+%% plot rotation curves
 
 % create new figure
 figure
@@ -164,21 +168,26 @@ errorbar(orbital_radii / meters_in_kiloparsec, orbital_velocities / 1000, orbita
 % add point for Sun
 errorbar(solar_orbital_radius / meters_in_kiloparsec, solar_orbital_velocity / 1000, solar_orbital_velocity_uncertainty / 1000, solar_orbital_velocity_uncertainty / 1000, solar_orbital_radius_uncertainty / meters_in_kiloparsec, solar_orbital_radius_uncertainty / meters_in_kiloparsec, 'o')
 
-%plot(2:8, (slope .* ((2*meters_in_kiloparsec):meters_in_kiloparsec:(8*meters_in_kiloparsec)) + y_intercept) / 1000)
+plot(0:8, (slope .* ((0*meters_in_kiloparsec):meters_in_kiloparsec:(8*meters_in_kiloparsec)) + y_intercept) / 1000)
 
 % plot theoretical rotation curve
 % plot inside inner sphere
-plot(inner_radii / meters_in_kiloparsec, galactic_rotational_velocity(inner_radii, mass_at_2_kpc .* (inner_radii / max(inner_radii)).^3) / 1000, 'b');
+plot(inner_radii / meters_in_kiloparsec, galactic_rotational_velocity(inner_radii, masses(2) .* (inner_radii / max(inner_radii)).^3) / 1000, '-r');
+%errorbar(inner_radii / meters_in_kiloparsec, galactic_rotational_velocity(inner_radii, masses(2) .* (inner_radii / max(inner_radii)).^3) / 1000, galactic_rotational_velocity(inner_radii, masses(2) - masses_uncertainties(2) .* (inner_radii / max(inner_radii)).^3) / 1000, galactic_rotational_velocity(inner_radii, masses(2) + masses_uncertainties(2) .* (inner_radii / max(inner_radii)).^3) / 1000, '-r');
 
 % plot outside inner sphere
-plot(outer_radii / meters_in_kiloparsec, galactic_rotational_velocity(outer_radii, mass_at_2_kpc) / 1000, 'b');
+plot(outer_radii / meters_in_kiloparsec, galactic_rotational_velocity(outer_radii, masses(2)) / 1000, '-r');
+%errorbar(outer_radii / meters_in_kiloparsec, galactic_rotational_velocity(outer_radii, masses(2)) / 1000, galactic_rotational_velocity(outer_radii, masses(2) - masses_uncertainties(2)) / 1000, galactic_rotational_velocity(outer_radii, masses(2) + masses_uncertainties(2)) / 1000, '-r');
 
 % add labels
 title('Derived Rotation Curve');
 xlabel('Orbital Radius (kpc)');
 ylabel('Orbital Velocity (km/s)');
 
+% add ylimit
+ylim([0, 350]);
+
 % add legend
-legend('HI Clouds', 'Sun', 'Location', 'northwest');
+legend('HI Clouds', 'Sun', 'Bisquare Fit', '2 kpc Central Mass', 'Location', 'northwest');
 
 hold off;
