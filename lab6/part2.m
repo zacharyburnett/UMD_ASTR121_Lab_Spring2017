e8ba889a5607b5e14c448b9a5f4061b19aa4125e
Zachary Burnett <zrb@umd.edu>
2017-05-03 19:58:31 -0400

    combined all data into one struct and changed plotting colors

--- lab6/part2.m
+++ lab6/part2.m
@@ -1,65 +1,21 @@
-%All wavelength info is the same for each. instensity
-%values are in the 'data' field of the struct
+% All wavelength info is the same for each. 
+% intensity values are in the 'data' field of the struct
+
+% define wavelengths in angstroms
 wavelengths = 3650:2:7100;
 
-% for NGC1832
-figure
-intensities1832 = NGC1832.data;
-plot(wavelengths, intensities1832);
+% get fieldnames (galaxy names)
+galaxy_names = fieldnames(galaxy_data_struct);
 
-%etc...
-figure
-intensities2276 = NGC2276.data;
-plot(wavelengths, intensities2276);
+% define plotting colors
+colors = jet(length(galaxy_names));
 
-figure
-intensities2798 = NGC2798.data;
-plot(wavelengths, intensities2798);
+for index = 1:numel(galaxy_names)
+    %figure
+    hold on
+    plot(wavelengths, galaxy_data_struct.(galaxy_names{index}).data, 'color', colors(index, :));
+    hold off
+    %title(galaxy_names{index});
+end
 
-figure
-intensities2903 = NGC2903.data;
-plot(wavelengths, intensities2903);
-
-figure
-intensities3034 = NGC3034.data;
-plot(wavelengths, intensities3034);
-
-figure
-intensities3077 = NGC3077.data;
-plot(wavelengths, intensities3077);
-
-figure
-intensities3147 = NGC3147.data;
-plot(wavelengths, intensities3147);
-
-figure
-intensities3368 = NGC3368.data;
-plot(wavelengths, intensities3368);
-
-figure
-intensities3627 = NGC3627.data;
-plot(wavelengths, intensities3627);
-
-figure
-intensities4750 = NGC4750.data;
-plot(wavelengths, intensities4750);
-
-figure
-intensities4775 = NGC4775.data;
-plot(wavelengths, intensities4775);
-
-figure
-intensities5195 = NGC5195.data;
-plot(wavelengths, intensities5195);
-
-figure
-intensities5248 = NGC5248.data;
-plot(wavelengths, intensities5248);
-
-figure
-intensities6181 = NGC6181.data;
-plot(wavelengths, intensities6181);
-
-figure
-intensities6643 = NGC6643.data;
-plot(wavelengths, intensities6643);
+legend(galaxy_names);
